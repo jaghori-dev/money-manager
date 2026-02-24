0df7d5903af832faa33178e570575e5b6a5faa4d
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-24 16:13:30 +0100

    remove unnecessary codes

--- pages/[id].js
+++ pages/[id].js
@@ -3,26 +3,22 @@ import { useRouter } from "next/router";
 import TransactionItem from "@/components/TransactionList/TransactionItem/TransactionItem";
 import useSWR from "swr";
 
-
-const fetcher = (url)=> fetch(url).then((res)=> res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function Details() {
   const router = useRouter();
   const { id } = router.query;
   if (!id) return <h1>Loading...</h1>;
 
-  const {data: transaction, error, isLoading } = useSWR(`/api/transactions/${id}`, fetcher)
+  const {
+    data: transaction,
+    error,
+    isLoading,
+  } = useSWR(`/api/transactions/${id}`, fetcher);
   if (isLoading) {
-    return <h1>Loading...</h1>;
+    return <h2>Loading...</h2>;
   }
-   if (!transaction) return <h2>transaction not found</h2>
-  console.log(transaction)
+  if (!transaction) return <h2>transaction not found</h2>;
 
 
-  return(
-    <TransactionItem transaction={transaction} isDetails={true}/>
-  );
+  return <TransactionItem transaction={transaction} isDetails={true} />;
 }
-
-const Title = styled.h1`
-  color: var(--main-color);
-`;
