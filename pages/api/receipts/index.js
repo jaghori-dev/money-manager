d634cc6cee5ea468923172e20dc2c3d36796abf5
Sergey Badin <sergeykfg1@me.com>
2026-02-26 12:22:13 +0100

    feat: add receipt uplouad api

---
+++ pages/api/receipts/index.js
@@ -0,0 +1,9 @@
+export default async function handler(request, response) {
+  if (request.method !== "POST") {
+    return response.status(405).json({ status: "Method not allowed" });
+  }
+
+  console.log("receipt hit");
+
+  return response.status(200).json({ status: "ok" });
+}
