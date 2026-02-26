25aa8020570f22a5beed5f3df5ea7c6a9380c3e2
Sergey Badin <sergeykfg1@me.com>
2026-02-26 10:35:39 +0100

    feat: add receiptURL field to transaction schema DB

--- db/models/Transaction.js
+++ db/models/Transaction.js
@@ -7,6 +7,7 @@ const transactionSchema = new Schema(
     amount: { type: Number, required: true },
     category: { type: String },
     date: { type: Date, default: Date.now },
+    receiptUrl: { type: String, default: "" },
   },
   { timestamps: true }
 );
