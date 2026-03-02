d56ac6b5f326e4481a5de50a36a9b747537dd83a
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-02 15:38:24 +0100

    add login component and import it to appjs

--- pages/_app.js
+++ pages/_app.js
@@ -2,28 +2,32 @@ import GlobalStyle from "../styles";
 import useSWR from "swr";
 import Navigation from "@/components/Navigation/Navigation";
 import { SessionProvider } from "next-auth/react";
+import Login from "@/components/Login";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   const {
     data: transactions,
     error,
     isLoading,
   } = useSWR("/api/transactions", fetcher);
   const reversedTransactions = transactions?.slice().reverse();
+
   return (
-    <>
-      <SessionProvider session={session}>
-        <GlobalStyle />
-        <Component
-          {...pageProps}
-          transactions={reversedTransactions}
-          error={error}
-          isLoading={isLoading}
-        />
-        <Navigation />
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      <GlobalStyle />
+      <Login />
+      <Component
+        {...pageProps}
+        transactions={reversedTransactions}
+        error={error}
+        isLoading={isLoading}
+      />
+      <Navigation />
+    </SessionProvider>
   );
 }
